7b827edc765ff4c360df7cecac21c04d5d40147a
Eric <ericemeka12@gmail.com>
2025-09-16 02:03:27 +0100

    File Name Changed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { fetchAnalyticsData } from "../actions/analyticActions";
+import React from 'react'
 
-export default async function page() {
-  const { data } = await fetchAnalyticsData("2025-09-15","2025-09-21", 10);
-  console.log(data);
-  return <div>page</div>;
+export default function page() {
+  return (
+    <div>page</div>
+  )
 }
