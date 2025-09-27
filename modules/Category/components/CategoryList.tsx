9d29140c5d7a0bdaf9e64c843c4c61303db533f9
Eric <ericemeka12@gmail.com>
2025-09-27 20:43:06 +0100

    Bg Gradient removed from employee modal

--- modules/Category/components/CategoryList.tsx
+++ modules/Category/components/CategoryList.tsx
@@ -9,7 +9,7 @@ export default function CategoryList() {
   const categories = filteredCategories();
   return (
     <FadeInContainer>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
+      <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
         <AddCategory />
         {categories?.map((category) => (
           <CategoryCard {...category} key={category.id} />
--- modules/MenuItem/components/MenuItemList.tsx
+++ modules/MenuItem/components/MenuItemList.tsx
@@ -8,7 +8,7 @@ export default function MenuItemList() {
   const { menuItems } = useMenuItemDataStore();
   return (
     <FadeInContainer>
-      <div className="grid gap-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid gap-2 grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <AddMenuItemCard />
         {menuItems.map((menuItem) => (
           <MenuItemCard menuItem={menuItem} key={menuItem.id} />
